9f6cee001cd2e72c101378bb9b5811e099d02b8a
luqhman <luqhman2vish@gmail.com>
2023-12-11 07:59:52 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.01",
+    author="luqhman",
+    author_email="luqhman2vish@gmail.com",
+    pakages=find_packages(),
+    install_requires=get_requirements(),
+)
